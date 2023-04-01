8a9013da9f9b03ff25a946a38953294ea8bba644
John Rearden <reardenjohn@gmail.com>
2023-04-01 18:36:36 +0100

    Add check for server hostname in env.py in ALLOWED_HOSTS

--- crossword/settings.py
+++ crossword/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+SERVER_HOSTNAME = os.environ['SERVER_HOSTNAME'] or ''
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', SERVER_HOSTNAME]
 
 
 # Application definition
