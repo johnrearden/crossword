d0e56c861c6de28eb5a129c6a648f8e83cb7de3a
John Rearden <reardenjohn@gmail.com>
2023-05-12 21:57:21 +0100

    Add api endpoint to create new user

--- client_auth/urls.py
+++ client_auth/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('confirm_credentials/', views.ConfirmCredentialsView.as_view(),
          name='confirm_credentials'),
+    path('create_user/', views.CreateUserView.as_view(),
+         name='confirm_credentials'),
 ]
--- client_auth/views.py
+++ client_auth/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
 
@@ -20,3 +21,35 @@ class ConfirmCredentialsView(APIView):
             return Response(serializer.data)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class CreateUserView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        body = request.data
+        username = body['username']
+        email = body['email']
+        password = body['password']
+
+        existing_username = User.objects.filter(username=username)
+        if existing_username:
+            data = {'USER_ALREADY_EXISTS': 'USERNAME'}
+            return Response(data=data)
+
+        if email:
+            existing_email = User.objects.filter(email=email)
+            if existing_email:
+                print(existing_email)
+                data = {'USER_ALREADY_EXISTS': 'EMAIL'}
+                return Response(data=data)
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+
+        serializer = UserSerializer(instance=user)
+        return Response(serializer.data)
