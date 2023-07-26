15cdbdb2b02825d6434394509c330668286cbe5f
John Rearden <reardenjohn@gmail.com>
2023-07-26 19:35:19 +0100

    Add get parameter handling to accept list of seen cranagrams

--- crossword_client_api/urls.py
+++ crossword_client_api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('get_example/', views.GetExample.as_view(),
          name='builder_home'),
-    path('get_example_crannagram/', views.GetExampleCrannagram.as_view(),
+    path('get_example_crannagram/',
+         views.GetExampleCrannagram.as_view(),
          name='get_example_crannagram')
 ]
--- crossword_client_api/views.py
+++ crossword_client_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from builder.models import CrosswordPuzzle, CrosswordClue, PuzzleType
 from builder.serializers import CrosswordPuzzleSerializer,\
       CrosswordClueSerializer
+import random
 
 
 class GetExample(APIView):
@@ -13,7 +14,8 @@ class GetExample(APIView):
     def get(self, request):
         crossword = CrosswordPuzzle.objects \
             .order_by('-last_edited') \
-            .filter(released=True)[0]
+            .filter(released=True) \
+            .filter(puzzle_type=PuzzleType.CROSSWORD)[0]
         clues = CrosswordClue.objects.filter(puzzle=crossword)
         puzzle_serializer = CrosswordPuzzleSerializer(crossword)
         clue_serializer = CrosswordClueSerializer(clues, many=True)
@@ -30,15 +32,24 @@ class GetExampleCrannagram(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        cranagram = CrosswordPuzzle.objects \
+        params = request.GET["seen_cranagrams"].split(',')
+        print(params)
+
+        seen_cranagrams = [] if params == [''] else [int(p) for p in params]
+        cranagrams = CrosswordPuzzle.objects \
             .filter(puzzle_type=PuzzleType.CRANAGRAM) \
-            .order_by('-last_edited')[0]
-        clues = CrosswordClue.objects.filter(puzzle=cranagram)
-        puzzle_serializer = CrosswordPuzzleSerializer(cranagram)
-        clue_serializer = CrosswordClueSerializer(clues, many=True)
-        data = {
-            'puzzle': puzzle_serializer.data,
-            'clues': clue_serializer.data,
-        }
+            .exclude(id__in=seen_cranagrams) \
+            .order_by('-last_edited')
+        if len(cranagrams) == 0:
+            return Response({'data': None})
+        else:
+            cranagram = random.choice(cranagrams)
+            clues = CrosswordClue.objects.filter(puzzle=cranagram)
+            puzzle_serializer = CrosswordPuzzleSerializer(cranagram)
+            clue_serializer = CrosswordClueSerializer(clues, many=True)
+            data = {
+                'puzzle': puzzle_serializer.data,
+                'clues': clue_serializer.data,
+            }
 
         return Response({'data': data})
